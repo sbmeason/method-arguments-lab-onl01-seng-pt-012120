1f75b807d64a26c5a7e7d36e15e4ca0dd4e462a5
Samantha Meason <sbmeason@gmail.com>
2020-01-24 21:45:57 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -14,4 +14,4 @@ def introduction_with_language
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language ()
+introduction_with_language ("Josh", "Ruby")
