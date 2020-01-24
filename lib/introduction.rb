dccb6e2a3962c458f06abb3fb2395178a689ebde
Samantha Meason <sbmeason@gmail.com>
2020-01-24 21:46:18 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,17 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+def introduction_with_language
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+introduction("Josh")
+
+def introduction_with_language
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language ("Josh", "Ruby")
